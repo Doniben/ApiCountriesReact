42a9ddaf303479d9dc0c9eece9ad6d6d281d766b
Doniben <doniben@esperanto.co>
2021-02-01 11:18:11 -0500

    [FEATURE] hero component

---
+++ api-countries/src/components/Navbar.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const Navbar = () => {
+    return (
+        <nav className="nav">
+            <div>
+                <h1>Where in the World?</h1>
+            </div>
+            <i className="fas fa-moon"><span className="span"> Dark Mode</span></i>
+        </nav>
+    )
+}
+
+export default Navbar
