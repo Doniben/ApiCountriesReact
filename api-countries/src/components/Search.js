19ac21c228c958039248f955a7333e6190adfeaf
Doniben <doniben@esperanto.co>
2021-02-01 22:40:12 -0500

    [FEATURE] search filter working, but so slowly, the region filter doesn't work yet

--- api-countries/src/components/Search.js
+++ api-countries/src/components/Search.js
@@ -1,10 +1,27 @@
 import React from 'react'
 
 const Search = () => {
+    window.addEventListener('DOMContentLoaded', () => {
+        const search = document.getElementById('search')
+    
+        search.addEventListener('click', (e) => {
+          const { value } = e.target
+    
+          const countryName = document.querySelectorAll('.country-name')
+    
+          countryName.forEach((name) => {
+            if (name.innerText.toLowerCase().includes(value.toLowerCase())) {
+              name.parentElement.parentElement.style.display = 'block'
+            } else {
+              name.parentElement.parentElement.style.display = 'none'
+            }
+          })
+        })
+      })
     return (
         <section className="search-options">
             <form action="" className="form-control">
-                <input type="search" placeholder="Search for a country"/>
+                <input type="search" id="search" placeholder="Search for a country"/>
             </form>
             <div className="filter-option">
                 <select name="option" id="option">
